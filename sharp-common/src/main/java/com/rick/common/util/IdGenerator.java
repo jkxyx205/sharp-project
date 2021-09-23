d722c9bd4787126cead1636b3bb28ab712ba8ff8
rick <1050216579@qq.com>
2021-09-23 22:44:51 +0800

    feat: 添加IdGenerator

---
+++ sharp-common/src/main/java/com/rick/common/util/IdGenerator.java
@@ -0,0 +1,25 @@
+package com.rick.common.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Random;
+
+/**
+ * @author Rick
+ * @createdAt 2021-09-23 21:31:00
+ */
+@UtilityClass
+public class IdGenerator {
+
+    /**
+     * 生成主键(19位数字)
+     * 主键生成方式,年月日时分秒毫秒的时间戳+三位随机数保证不重复
+     */
+    public static long getId() {
+        //当前系统时间戳精确到毫秒
+        Long simple = System.currentTimeMillis();
+        //随机数
+        int random = new Random().nextInt(900) + 100;//为变量赋随机值100-999;
+        return Long.parseLong( simple.toString() + random);
+    }
+}
