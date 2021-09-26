33cdad8e40196544f3798a6881c497cfd2903d2c
rick <1050216579@qq.com>
2021-09-26 11:40:51 +0800

    feat: 添加sequence，https://gitee.com/yu120/sequence/blob/master/src/main/java/cn/ms/sequence/Sequence.java

--- sharp-common/src/main/java/com/rick/common/util/IdGenerator.java
+++ sharp-common/src/main/java/com/rick/common/util/IdGenerator.java
@@ -1,5 +1,6 @@
 package com.rick.common.util;
 
+import com.rick.common.util.sequence.Sequence;
 import lombok.experimental.UtilityClass;
 
 import java.util.Random;
@@ -11,15 +12,22 @@ import java.util.Random;
 @UtilityClass
 public class IdGenerator {
 
+    private static final Sequence sequence =   new Sequence(0);
+
     /**
      * 生成主键(19位数字)
      * 主键生成方式,年月日时分秒毫秒的时间戳+三位随机数保证不重复
      */
-    public static long getId() {
+    public static long getSimpleId() {
         //当前系统时间戳精确到毫秒
         Long simple = System.currentTimeMillis();
         //随机数
         int random = new Random().nextInt(900) + 100;//为变量赋随机值100-999;
         return Long.parseLong( simple.toString() + random);
     }
+
+    public static long getSequenceId() {
+        return sequence.nextId();
+    }
+
 }
