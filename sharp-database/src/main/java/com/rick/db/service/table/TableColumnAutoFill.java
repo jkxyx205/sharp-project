44c100cee61b2027da0ed190ba59de172ea2e001
Rick <jkxyx205@163.com>
2021-09-27 03:22:09 +0800

    docs: 添加注释

--- sharp-database/src/main/java/com/rick/db/service/table/TableColumnAutoFill.java
+++ sharp-database/src/main/java/com/rick/db/service/table/TableColumnAutoFill.java
@@ -7,5 +7,9 @@ import java.util.Map;
  * @createdAt 2021-09-26 10:21:00
  */
 public interface TableColumnAutoFill {
+    /**
+     * 将填充的字段返回
+     * @return
+     */
     Map<String, Object> fill();
 }
