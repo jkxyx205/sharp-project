5164843d287198247ac6967bb9ba16cbab064c09
Rick <jkxyx205@163.com>
2021-10-27 22:46:38 +0800

    chore：添加Assert类

--- sharp-common/src/main/java/com/rick/common/util/Assert.java
+++ sharp-common/src/main/java/com/rick/common/util/Assert.java
@@ -1,16 +1,34 @@
 package com.rick.common.util;
 
-import org.springframework.lang.Nullable;
-
 /**
  * @author Rick
  * @createdAt 2021-10-27 09:48:00
  */
 public abstract class Assert {
 
-    public static void notNull(@Nullable Object object, String message) {
+    public static void notNull(Object object, String message) {
         if (object == null) {
+            throw new IllegalArgumentException(message);
+
+        }
+    }
+
+    public static void hasText(String text, String message) {
+        if (!(text != null && !text.isEmpty() && containsText(text))) {
             throw new IllegalArgumentException(message);
         }
     }
+
+    private static boolean containsText(CharSequence str) {
+        int strLen = str.length();
+
+        for(int i = 0; i < strLen; ++i) {
+            if (!Character.isWhitespace(str.charAt(i))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
