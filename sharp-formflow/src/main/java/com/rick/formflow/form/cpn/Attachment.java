554cb8e378627f89d671a5d804311d6c06e1aff4
Rick <jkxyx205@163.com>
2021-11-08 18:48:20 +0800

    feat: 添加文件控件

--- sharp-formflow/src/main/java/com/rick/formflow/form/cpn/Attachment.java
+++ sharp-formflow/src/main/java/com/rick/formflow/form/cpn/Attachment.java
@@ -2,20 +2,23 @@ package com.rick.formflow.form.cpn;
 
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.rick.common.util.JsonUtils;
 import com.rick.formflow.form.cpn.core.AbstractCpn;
 import com.rick.formflow.form.cpn.core.CpnTypeEnum;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Rick
  * @createdAt 2021-11-02 17:11:00
  */
 @Component
-public class Attachment extends AbstractCpn<List<String>> {
+public class Attachment extends AbstractCpn<List<Map<String, Object>>> {
 
     @Override
     public CpnTypeEnum getCpnType() {
@@ -23,14 +26,24 @@ public class Attachment extends AbstractCpn<List<String>> {
     }
 
     @Override
-    public List<String> httpConverter(Object value) {
+    public List<Map<String, Object>> httpConverter(Object value) {
         if (value instanceof String) {
             if (StringUtils.isBlank((CharSequence) value)) {
-                return Collections.emptyList();
+                return null;
             }
-            return Lists.newArrayList((String) value);
+            try {
+                List<Map> list = JsonUtils.toList((String) value, Map.class);
+                List<Map<String,  Object>> result = Lists.newArrayListWithExpectedSize(list.size());
+                for (Map map : list) {
+                    result.add(Maps.newHashMap(map));
+                }
+                return result;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
         }
 
-        return (List<String>) value;
+        return (List<Map<String, Object>>) value;
     }
 }
