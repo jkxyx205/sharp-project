08bbf90391ce84fff6cf792e45d5c21047e7dea2
Rick <jkxyx205@163.com>
2022-04-15 22:53:33 +0800

    fix: change assert exception

--- sharp-common/src/main/java/com/rick/common/http/exception/Assert.java
+++ sharp-common/src/main/java/com/rick/common/http/exception/Assert.java
@@ -26,20 +26,26 @@ public interface Assert {
         throw new BizException(new ExceptionResult<String>(getCode(), String.format(getMsg(), params)));
     }
 
-    static void isNull(Object obj, String name) {
+    static void isNull(Object obj, String message) {
         if (Objects.nonNull(obj)) {
-            throw new BizException(new ExceptionResult<String>(400, String.format("%s需要为空", new String[]{name})));
+            throw new IllegalArgumentException(message);
         }
     }
 
-    static void notNull(Object obj, String name) {
+    static void notNull(Object obj, String message) {
         if (Objects.isNull(obj)) {
-            throw new BizException(new ExceptionResult<String>(400, String.format("%s不能为空", new String[]{name})));
+            throw new IllegalArgumentException(message);
         }
     }
 
-    static void notExists(String name) {
-        throw new BizException(new ExceptionResult<String>(404, String.format("%s不存在", new String[]{name})));
+    static void notExists(String message) {
+        throw new BizException(new ExceptionResult<String>(404, message));
+    }
+
+    static void state(boolean expression, String message) {
+        if (!expression) {
+            throw new IllegalStateException(message);
+        }
     }
 
     int getCode();
