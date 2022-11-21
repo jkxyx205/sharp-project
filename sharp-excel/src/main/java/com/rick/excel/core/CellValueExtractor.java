7066cb73e3101199871cb03f8ae3a28fb757149a
Rick <jkxyx205@163.com>
2022-11-21 18:58:11 +0800

    fix: ExcelReader lost row.

---
+++ sharp-excel/src/main/java/com/rick/excel/core/CellValueExtractor.java
@@ -0,0 +1,39 @@
+package com.rick.excel.core;
+
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+
+/**
+ * 单元格值转化接口
+ * @author Rick
+ * @createdAt 2022-11-21 09:54:00
+ */
+public interface CellValueExtractor {
+
+    /**
+     * 根据XSSFCell类型设置数据
+     * @param cell
+     * @return
+     */
+    default Object getCellValue(XSSFCell cell) {
+        if (cell == null) {
+            return null;
+        }
+
+        switch (cell.getCellType()) {
+            case NUMERIC: {
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    return cell.getDateCellValue();
+                }
+                return cell.getNumericCellValue();
+            }
+            case BOOLEAN:
+                return cell.getBooleanCellValue();
+            case _NONE:
+            case ERROR:
+                return null;
+            default:
+                return cell.getStringCellValue();
+        }
+    }
+}
--- sharp-excel/src/main/java/com/rick/excel/core/ExcelReader.java
+++ sharp-excel/src/main/java/com/rick/excel/core/ExcelReader.java
@@ -1,8 +1,5 @@
 package com.rick.excel.core;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.ss.usermodel.DateUtil;
-import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -10,88 +7,73 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.InputStream;
 
 /**
- * 
+ * Excel读取
  * @author Rick.Xu
  * @Date 2014-12-16
  *
  */
-@Slf4j
 public class ExcelReader {
+
 	/**
 	 * @param is
 	 * @param ers
-	 * @return 总的行数
+	 * @return 读取excel
 	 * @throws Exception
 	 */
 	public static void readExcelContent(InputStream is, ExcelResultSet ers) throws Exception {
+		readExcelContent(is, ers, new CellValueExtractor() {});
+	}
+
+	/**
+	 * @param is
+	 * @param ers
+	 * @return 读取excel
+	 * @throws Exception
+	 */
+	public static void readExcelContent(InputStream is, ExcelResultSet ers, CellValueExtractor cellValueExtractor) throws Exception {
 		ers.beforeReader();
 
 		XSSFWorkbook wb = new XSSFWorkbook(is);
 		XSSFSheet sheet;
 		XSSFRow row;
-		
-		int sheetIndexs = wb.getNumberOfSheets();
-		
-		for (int sheetIndex = 0; sheetIndex < sheetIndexs; sheetIndex++) {
+
+		int sheetIndexes = wb.getNumberOfSheets();
+
+		for (int sheetIndex = 0; sheetIndex < sheetIndexes; sheetIndex++) {
 			sheet = wb.getSheetAt(sheetIndex);
-	        // 得到总行数
-	        int rowNum = sheet.getLastRowNum();
-//	        row = sheet.getRow(0);
-	        row = sheet.getRow(sheet.getFirstRowNum());
-	        if (row == null) {
-	        	break;
+			// 得到总行数
+			int rowNum = sheet.getLastRowNum();
+			row = sheet.getRow(sheet.getFirstRowNum());
+			if (row == null) {
+				break;
 			}
 
-	        String sheetName = sheet.getSheetName();
-	        int colNum = row.getPhysicalNumberOfCells();
-	        // 正文
-	        for (int i = 0; i <= rowNum; i++) {
-	            row = sheet.getRow(i);
-	            if(row == null)
-	            	continue;
-	            
-	            int j = 0;
-	            Object[] data = new Object[colNum];
-	            while (j < colNum) {
-					data[j] = getCellValue(row.getCell(j));
-	                j++;
-	            }
-	            
-	            if(!ers.rowMapper(i, data, sheetIndex,sheetName)) {
-	            	break;
-	            }
-	        }
-		 }
-		ers.afterReader();
-    }
-  
-	/**
-     * 根据XSSFCell类型设置数据
-     * @param cell
-     * @return
-     */
-    private static Object getCellValue(XSSFCell cell) {
-    	if (cell == null) {
-    		return null;
-		}
+			String sheetName = sheet.getSheetName();
 
-		switch (cell.getCellType()) {
-			case NUMERIC: {
-				if (DateUtil.isCellDateFormatted(cell)) { // 日期
-					return cell.getDateCellValue();
+			// 正文
+			for (int i = 0; i <= rowNum; i++) {
+				row = sheet.getRow(i);
+				Object[] data;
+				if(row == null) {
+					data = new Object[]{};
+				} else {
+					int colNum = row.getPhysicalNumberOfCells();
+					int j = 0;
+					data = new Object[colNum];
+					while (j < colNum) {
+						data[j] = cellValueExtractor.getCellValue(row.getCell(j));
+						j++;
+					}
 				}
-//				return BigDecimal.valueOf(cell.getNumericCellValue());
-				return cell.getNumericCellValue();
+
+				if(!ers.rowMapper(i, data, sheetIndex,sheetName)) {
+					break;
+				}
 			}
-			case FORMULA:
-				return cell.getCellFormula();
-			case BOOLEAN:
-				return cell.getBooleanCellValue();
-			case _NONE:
-			case ERROR:
-				return null;
-			default:
-				return cell.getStringCellValue();
 		}
-    }
+		ers.afterReader();
+		is.close();
+		wb.close();
+	}
+
 }
