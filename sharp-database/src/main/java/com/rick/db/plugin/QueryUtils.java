107f6b43d7503a1e7823840a20699f5d8a2c2834
rick <jkxyx205@163.com>
2023-05-15 18:11:30 +0800

    refactor: code optimize

--- sharp-database/src/main/java/com/rick/db/plugin/QueryUtils.java
+++ sharp-database/src/main/java/com/rick/db/plugin/QueryUtils.java
@@ -6,10 +6,8 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.util.ReflectionUtils;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.io.Serializable;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -34,7 +32,7 @@ public final class QueryUtils {
     }
 
     /**
-     *  select id, name, grade, sex from t_school_student where school_id IN (552173736070144000);
+     *  SELECT id, name, grade, sex FROM t_school_student WHERE school_id IN (552173736070144000);
      *
      * @param selectColumnNames id, name, grade, sex
      * @param queryObject t_school_student
@@ -45,7 +43,7 @@ public final class QueryUtils {
      * @param <T>
      */
     public static <T> Map<?, List<T>> subTableValueMap(String selectColumnNames, String queryObject, String refColumnName, Collection<?> refValue, Class<T> tClass) {
-        List<T> list = sharpService.query("select " + selectColumnNames + " from " + queryObject + " where " + refColumnName + " IN (:refColumnName)", Params.builder(1).pv("refColumnName", refValue).build(), tClass);
+        List<T> list = subTableValueList(selectColumnNames, queryObject, refColumnName, refValue, tClass);
         if (CollectionUtils.isEmpty(list)) {
             return Collections.emptyMap();
         }
@@ -61,5 +59,29 @@ public final class QueryUtils {
 
         Map<?, List<T>> map = list.stream().collect(Collectors.groupingBy(t -> ReflectionUtils.invokeMethod(ReflectionUtils.findMethod(tClass, "get" + groupPropertyName.substring(0, 1).toUpperCase() + groupPropertyName.substring(1)), t), Collectors.mapping(Function.identity(), Collectors.toList())));
         return map;
+    }
+
+    public static <T> List<T> subTableValueList(String selectColumnNames, String queryObject, String refColumnName, Serializable refValue, Class<T> tClass) {
+        return subTableValueList(selectColumnNames, queryObject, refColumnName, Arrays.asList(refValue), tClass);
+    }
+
+    /**
+     *  SELECT id, name, grade, sex FROM t_school_student WHERE school_id IN (552173736070144000);
+     *
+     * @param selectColumnNames id, name, grade, sex
+     * @param queryObject t_school_student
+     * @param refColumnName school_id
+     * @param refValue [552173736070144000]
+     * @param tClass 子表返回的bean
+     * @return list List<tClass>
+     * @param <T>
+     */
+    public static <T> List<T> subTableValueList(String selectColumnNames, String queryObject, String refColumnName, Collection<?> refValue, Class<T> tClass) {
+        List<T> list = sharpService.query("SELECT " + selectColumnNames + " FROM " + queryObject + " WHERE " + refColumnName + " IN (:refColumnName)", Params.builder(1).pv("refColumnName", refValue).build(), tClass);
+        if (CollectionUtils.isEmpty(list)) {
+            return Collections.emptyList();
+        }
+
+        return list;
     }
 }
