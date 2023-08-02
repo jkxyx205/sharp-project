e92c34180645168fd4b42f73ea7cb6e1c4ed55b3
rick <jkxyx205@163.com>
2023-08-02 22:07:17 +0800

    refactor: editable-table.js 插件优化.添加只读属性

--- sharp-formflow/src/main/resources/static/editable-table/editable-table.js
+++ sharp-formflow/src/main/resources/static/editable-table/editable-table.js
@@ -16,11 +16,13 @@ head.appendChild(style)
         init: function() {
             this.$tbody = this.$element.find('tbody')
             if (this.options.value) {
-                _renderTable(this.$tbody, this.options.value, this.options.readonly)
+                _renderTable(this.$tbody, this.options.value)
             }
 
-            if (!this.options.readonly) {
-                _addEmptyLine(this.options.columns, this.$tbody, this.options.addEmptyLineCallback, this.options.beforeRemoveCallback, this.options.afterRemoveCallback, this.options.readonly)
+            _addEmptyLine(this.options.columns, this.$tbody, this.options.addEmptyLineCallback, this.options.beforeRemoveCallback, this.options.afterRemoveCallback)
+
+            if (this.options.readonly !== undefined) {
+                this.readonly(this.options.readonly)
             }
         },
         getValue: function() {
@@ -35,11 +37,24 @@ head.appendChild(style)
             return list
         },
         addEmptyLine: function () {
-            _addEmptyLine(this.options.columns, this.$tbody, this.options.addEmptyLineCallback, this.options.beforeRemoveCallback, this.options.afterRemoveCallback, this.options.readonly)
-        }
+            _addEmptyLine(this.options.columns, this.$tbody, this.options.addEmptyLineCallback, this.options.beforeRemoveCallback, this.options.afterRemoveCallback)
+        },
+        readonly:function (readonly) {
+            this.options.readonly = (readonly === 'true' || readonly === true)
+            this.$tbody.find(':input').prop('disabled', this.options.readonly).prop('readonly', this.options.readonly)
+
+            if (this.$tbody.find('tr').length > 1) {
+                if (this.options.readonly) {
+                    this.$tbody.find('tr:not(:last-child) .operator, tr:last-child').hide()
+                } else {
+                    this.$tbody.find('tr:last-child').show()
+                    this.$tbody.find('tr:not(:last-child) .operator').css('display', 'table-cell')
+                }
+            }
+        },
     }
 
-    function _renderTable($tbody, list, readonly) {
+    function _renderTable($tbody, list) {
         var tbodyHTML = []
 
         for (arr of list) {
@@ -48,46 +63,36 @@ head.appendChild(style)
             for(v of arr) {
                 tbodyHTML.push('<td><input class="form-control" type="text" value="'+v+'" autocomplete="off"/></td>')
             }
-            if (!readonly) {
-                tbodyHTML.push('<td class="operator btn-link">X</td></tr>')
-            }
 
+            tbodyHTML.push('<td class="operator btn-link">X</td></tr>')
         }
+
         $tbody.append(tbodyHTML.join(''))
-
-        if (readonly) {
-            $tbody.find(':input').attr('disabled', true).attr('readonly', true)
-        }
     }
 
-    function input_focus(e, columns, $tbody, addEmptyLineCallback, beforeRemoveCallback, afterRemoveCallback, readonly) {
+    function input_focus(e, columns, $tbody, addEmptyLineCallback, beforeRemoveCallback, afterRemoveCallback) {
         let isLastChild = ($(e.target).parent().parent().next().length == 0)
         if (isLastChild) {
-            _addEmptyLine(columns, $tbody, addEmptyLineCallback, beforeRemoveCallback, afterRemoveCallback, readonly)
+            _addEmptyLine(columns, $tbody, addEmptyLineCallback, beforeRemoveCallback, afterRemoveCallback)
         }
     }
 
-    function _addEmptyLine(columns, $tbody, addEmptyLineCallback, beforeRemoveCallback, afterRemoveCallback, readonly) {
+    function _addEmptyLine(columns, $tbody, addEmptyLineCallback, beforeRemoveCallback, afterRemoveCallback) {
         var lineHTML = []
         lineHTML.push('<tr>')
         for(var i = 0; i < columns; i++) {
             lineHTML.push('<td><input class="form-control" type="text" value="" autocomplete="off"/></td>')
         }
 
-        if (!readonly) {
-            lineHTML.push('<td class="operator btn-link">X</td></tr>')
-        }
+
+        lineHTML.push('<td class="operator btn-link">X</td></tr>')
 
         $tbody.append(lineHTML.join(''))
         addEmptyLineCallback && addEmptyLineCallback($tbody.find('tr:last-child'))
-        _bindEvent(columns, $tbody, addEmptyLineCallback, beforeRemoveCallback, afterRemoveCallback, readonly)
+        _bindEvent(columns, $tbody, addEmptyLineCallback, beforeRemoveCallback, afterRemoveCallback)
     }
 
-    function _bindEvent(columns, $tbody, addEmptyLineCallback, beforeRemoveCallback, afterRemoveCallback, readonly) {
-        if (readonly) {
-            return
-        }
-
+    function _bindEvent(columns, $tbody, addEmptyLineCallback, beforeRemoveCallback, afterRemoveCallback) {
         $tbody.find('tr:last-child :input').each(function() {
             if (this.tagName === 'INPUT') {
                 this.addEventListener("input", function(e) {
@@ -142,6 +147,6 @@ head.appendChild(style)
     };
 
     $.fn.editableTable.defaults = {
-        readonly: false
+        readonly: undefined
     };
 })(jQuery);
