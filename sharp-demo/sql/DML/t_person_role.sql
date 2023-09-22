5a7410d7c410e8937434b0b0c54918d81adcb320
rick <jkxyx205@163.com>
2023-09-22 18:18:12 +0800

    test: 更新测试脚本

--- sharp-demo/sql/DML/t_person_role.sql
+++ sharp-demo/sql/DML/t_person_role.sql
@@ -1,2 +1,5 @@
 INSERT INTO `sharp-demo`.t_person_role (person_id, role_id, is_deleted) VALUES (552098712424472576, 530861443353075712, false);
 INSERT INTO `sharp-demo`.t_person_role (person_id, role_id, is_deleted) VALUES (552100575806939136, 530861443353075712, false);
+INSERT INTO `sharp-demo`.t_person_role (person_id, role_id, is_deleted) VALUES (736418716983508992, 530861443353075712, false);
+INSERT INTO `sharp-demo`.t_person_role (person_id, role_id, is_deleted) VALUES (736418717084172288, 530861443353075712, false);
+INSERT INTO `sharp-demo`.t_person_role (person_id, role_id, is_deleted) VALUES (736418717780426752, 530861443353075712, false);
