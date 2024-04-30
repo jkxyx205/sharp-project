c98e851d5b78263aaddae5ebdfb9ed9ca9a906c1
rick <jkxyx205@163.com>
2024-04-30 09:21:04 +0800

    test: 测试调整，添加新的 HTML 渲染项

--- sharp-admin/src/test/java/com/rick/admin/core/ReportRenderTest.java
+++ sharp-admin/src/test/java/com/rick/admin/core/ReportRenderTest.java
@@ -13,14 +13,13 @@ import java.util.Map;
 
 /**
  * @author Rick
- * @createdAt 2021-10-13 15:27:00
+ * @createdAt 2024-04-28 15:27:00
  */
 @SpringBootTest
 public class ReportRenderTest {
 
     @Autowired
     private ReportService reportService;
-
 
     /**
      * 添加字段
@@ -31,9 +30,7 @@ public class ReportRenderTest {
         int x = 0;
 
         Map<String, Object> params = new HashMap<>();
-
         ReportDTO reportDTO = reportService.list(reportId, new HashMap<>());
-
         Grid<Object[]> grid = reportDTO.getGridArray();
         params.put("report", reportDTO.getReport());
         params.put("grid", grid);
@@ -45,7 +42,7 @@ public class ReportRenderTest {
          </a>
          */
         // language=HTML
-        String html = "<tr th:each=\"m : ${grid.rows}\" th:if=\"${mStat.index == 0}\">\n" +
+        String bodyHtml = "<tr th:each=\"m : ${grid.rows}\" th:if=\"${mStat.index == 0}\">\n" +
                 "  <td x:text=\"${mStat.index + 1 + (grid.page - 1) * grid.pageSize}\"></td>\n" +
                 "  <td th:each=\"v : ${grid.rows[mStat.index]}\" th:with=\"index=${vStat.index + "+x+"}\" th:class=\"${'text-' + report.reportColumnList.get(vStat.index).align.name().toLowerCase()}\" th:if=\"${!report.reportColumnList.get(vStat.index).hidden}\">\n" +
                 "      <span th:name=\"${report.reportColumnList.get(vStat.index).name}\" th:if=\"${vStat.index > 1}\" th:attr=\"xtext=${'${grid.rows[mStat.index][' + index + ']}'}\"></span>\n" +
@@ -53,15 +50,55 @@ public class ReportRenderTest {
                 "  <input type=\"hidden\" x:each=\"v : ${grid.rows[mStat.index]}\" x:if=\"${report.reportColumnList.get(vStat.index).hidden}\" x:name=\"${report.reportColumnList.get(vStat.index).name}\" x:value=\"${v}\" />\n" +
                 "</tr>";
 
-        String htmlContent = ThymeleafRenderHelper.renderByHtmlContent(html, params).replaceAll("(?m)^[ \\t]*\\r?\\n","");
-//        System.out.println(htmlContent);
+        String bodyRenderedHtmlContent = ThymeleafRenderHelper.renderByHtmlContent(bodyHtml, params).replaceAll("(?m)^[ \\t]*\\r?\\n","");
+//        System.out.println(bodyRenderedHtmlContent);
 
         // add thymeleaf tag
-        htmlContent = htmlContent.replace("<tr>", "<tr th:each=\"m : ${grid.rows}\">")
+        bodyRenderedHtmlContent = bodyRenderedHtmlContent.replace("<tr>", "<tr th:each=\"m : ${grid.rows}\">")
                 .replace("xtext", "th:text")
                 .replace("x:", "th:");
 
-        System.out.println(htmlContent);
+        System.out.println(bodyRenderedHtmlContent);
+
+        System.out.println("========================================================================");
+
+        // header
+        String headerHtml = "<tr>\n" +
+                "    <th style=\"width: 40px\">序号</th>\n" +
+                "    <th th:each=\"n : ${report.reportColumnList}\" th:text=\"${n.label}\" th:data-name=\"${n.name}\" th:class=\"${(n.sortable ? 'sortable text-' + n.align.name().toLowerCase() : 'text-' + n.align.name().toLowerCase()) + (n.name == report.sidx ? ' active' : '') }\" th:style=\"${n.columnWidth == null ? '': 'width: ' + n.columnWidth + 'px'}\" th:if=\"${!n.hidden}\"></th>\n" +
+                "</tr>";
+
+        String headerRenderedHtmlContent = ThymeleafRenderHelper.renderByHtmlContent(headerHtml, params).replaceAll("(?m)^[ \\t]*\\r?\\n","");
+        System.out.println(headerRenderedHtmlContent);
+        System.out.println("========================================================================");
+
+        // form
+        /**
+         * 如果必要， 手动在 form 中添加 hidden input
+         *  <input type="hidden" name="batch_id" id="batch_id">
+         */
+        String formHtml = "<th:block th:each=\"f : ${report.queryFieldList}\" th:if=\"${report.queryFieldList != null && report.queryFieldList.size() > 0}\">\n" +
+                "    <div class=\"form-group\">\n" +
+                "        <label class=\"col-form-label mr-2\" th:for=\"${f.name}\" th:text=\"${f.label}\"></label>\n" +
+                "        <th:block th:if=\"${f.type.name() ne 'DATE_RANGE'}\">\n" +
+                "            <input class=\"form-control\" th:id=\"${f.name}\" type=\"text\" th:name=\"${f.name}\" autocomplete th:if=\"${f.type.name() == 'TEXT'}\" th:value=\"${f.value}\" th:placeholder=\"${f.placeholder}\">\n" +
+                "            <sp:select  th:id=\"${f.name}\" th:name=\"${f.name}\" th:attr=\"key=${f.extraData}\" class=\"form-control\" th:if=\"${f.type.name() == 'SELECT'}\"/>\n" +
+                "            <sp:select  th:id=\"${f.name}\" th:name=\"${f.name}\" th:attr=\"key=${f.extraData}\" class=\"form-control\" group th:if=\"${f.type.name() == 'GROUP_SELECT'}\"/>\n" +
+                "            <sp:select  th:id=\"${f.name}\" th:name=\"${f.name}\" multiple th:attr=\"key=${f.extraData}\" class=\"form-control\" th:if=\"${f.type.name() == 'MULTIPLE_SELECT'}\" hideAllItem/>\n" +
+                "        </th:block>\n" +
+                "\n" +
+                "        <th:block style=\"margin-bottom: 10px;\" th:if=\"${f.type.name() == 'DATE_RANGE'}\">\n" +
+                "            <div class=\"input-group input-daterange\" th:classappend=\"${f.name}\">\n" +
+                "                <input class=\"form-control\" th:id=\"${f.name + '0'}\" th:name=\"${f.name + '0'}\" type=\"text\" placeholder=\"开始时间\" autocomplete=\"off\">\n" +
+                "                <label class=\"col-form-label\" style=\"padding-left: 0;\">～</label>\n" +
+                "                <input class=\"form-control\" th:id=\"${f.name + '1'}\" th:name=\"${f.name + '1'}\" type=\"text\" placeholder=\"结束时间\" autocomplete=\"off\">\n" +
+                "            </div>\n" +
+                "        </th:block>\n" +
+                "    </div>\n" +
+                "</th:block>";
+
+        String formRenderedHtmlContent = ThymeleafRenderHelper.renderByHtmlContent(formHtml, params).replaceAll("(?m)^[ \\t]*\\r?\\n","");
+        System.out.println(formRenderedHtmlContent);
     }
 
 }
