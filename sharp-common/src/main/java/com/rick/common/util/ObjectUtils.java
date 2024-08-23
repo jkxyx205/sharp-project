a14d7c6ea395c5ffd578e566823615424954c35b
rick <jkxyx205@163.com>
2024-08-23 08:48:02 +0800

    chore: 代码优化

---
+++ sharp-common/src/main/java/com/rick/common/util/ObjectUtils.java
@@ -0,0 +1,51 @@
+package com.rick.common.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.time.temporal.Temporal;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @author Rick.Xu
+ * @date 2024/8/23 06:37
+ */
+@UtilityClass
+public class ObjectUtils {
+
+    public boolean mayPureObject(Class clazz) {
+        if (clazz == null) {
+            return false;
+        }
+
+        if (Number.class.isAssignableFrom(clazz)) {
+            return false;
+        } else if (CharSequence.class.isAssignableFrom(clazz)) {
+            return false;
+        } else if (clazz == Character.class) {
+            return false;
+        } else if (clazz == Boolean.class) {
+            return false;
+        } else if (clazz.isEnum()) {
+            return false;
+        } else if (clazz.isArray()) {
+            return false;
+        } else if (Temporal.class.isAssignableFrom(clazz)) {
+            return false;
+        } else if (clazz.isPrimitive()) {
+            return false;
+        } else if (Collection.class.isAssignableFrom(clazz) || Map.class.isAssignableFrom(clazz)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean mayPureObject(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        return mayPureObject(obj.getClass());
+    }
+}
