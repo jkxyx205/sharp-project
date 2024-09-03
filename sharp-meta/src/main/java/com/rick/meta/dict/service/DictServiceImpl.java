e392df177e63f04ed3ba541c3084761817fe47f0
rick <jkxyx205@163.com>
2024-09-03 15:10:31 +0800

    chore: 字典按照 sort 排序

--- sharp-meta/src/main/java/com/rick/meta/dict/service/DictServiceImpl.java
+++ sharp-meta/src/main/java/com/rick/meta/dict/service/DictServiceImpl.java
@@ -31,7 +31,7 @@ public class DictServiceImpl implements DictService, InitializingBean {
 
     private final DictDOSupplier dictDOSupplier;
 
-    private static final String SELECT_SQL = "SELECT type, name, label, sort FROM sys_dict WHERE type = :type";
+    private static final String SELECT_SQL = "SELECT type, name, label, sort FROM sys_dict WHERE type = :type ORDER BY sort";
 
     public Map<String, List<Dict>> getDictsByCodes(String... codes) {
         if (ArrayUtils.isNotEmpty(codes)) {
