28ae80847fc6bb41c7445e6cce795d28720ecdda
rick <jkxyx205@163.com>
2024-09-14 16:21:27 +0800

    chore: form 代码中可以引入 css 和 js 文件

--- sharp-report/src/main/java/com/rick/report/core/support/ReportConstants.java
+++ sharp-report/src/main/java/com/rick/report/core/support/ReportConstants.java
@@ -6,17 +6,21 @@ package com.rick.report.core.support;
  */
 public class ReportConstants {
 
-    public static final String ADDITIONAL_ENDPOINT= "endpoint";
+    public static final String ADDITIONAL_ENDPOINT = "endpoint";
 
-    public static final String ADDITIONAL_FORM_ID= "formId";
+    public static final String ADDITIONAL_FORM_ID = "formId";
 
-    public static final String ADDITIONAL_FORM_ACTION= "formAction";
+    public static final String ADDITIONAL_FORM_ACTION = "formAction";
 
-    public static final String ADDITIONAL_CSS= "css";
+    public static final String ADDITIONAL_LINK = "link";
 
-    public static final String ADDITIONAL_JS= "js";
+    public static final String ADDITIONAL_CSS = "css";
 
-    public static final String ADDITIONAL_OPERATOR_BAR= "operator-bar";
+    public static final String ADDITIONAL_SCRIPT = "script";
 
-    public static final String ADDITIONAL_JS_OPERATOR_COLUMN= "js-operator-column";
+    public static final String ADDITIONAL_JS = "js";
+
+    public static final String ADDITIONAL_OPERATOR_BAR = "operator-bar";
+
+    public static final String ADDITIONAL_JS_OPERATOR_COLUMN = "js-operator-column";
 }
