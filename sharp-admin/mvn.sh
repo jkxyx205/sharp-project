ef9c8855171f65ecfd29e7d6985875d3650a9ea4
rick <jkxyx205@163.com>
2024-09-22 17:46:31 +0800

    feat: 打包后自动上传并运行程序

--- sharp-admin/mvn.sh
+++ sharp-admin/mvn.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 mvn clean package -Dmaven.test.skip=true
+scp /Users/rick/Space/Workspace/sharp-project/sharp-admin/target/sharp-admin-1.0-SNAPSHOT.jar root@106.153.132.172:/usr/local/projects/sharp-admin/deploy
+
+#ssh dao
+# cd /usr/local/projects/sharp-admin
+# sh clean.sh
+#ps -ef | pgrep -f "8082"
+
+ssh dao "bash -s < /usr/local/projects/sharp-admin/clean.sh"
