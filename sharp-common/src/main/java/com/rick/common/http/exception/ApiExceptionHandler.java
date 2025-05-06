6af466f6179799cabb01868fa7a8e9921faff380
rick <jkxyx205@163.com>
2025-05-06 11:07:02 +0800

    fix: 日志打印优化

--- sharp-common/src/main/java/com/rick/common/http/exception/ApiExceptionHandler.java
+++ sharp-common/src/main/java/com/rick/common/http/exception/ApiExceptionHandler.java
@@ -25,9 +25,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -49,19 +47,19 @@ public class ApiExceptionHandler {
 
     /**
      * 业务异常
-     * @param ex
+     * @param e
      * @return
      */
     @ExceptionHandler(BizException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public Result exceptionHandler(HttpServletRequest request, HttpServletResponse response, BizException ex) throws IOException, ServletException {
-        Result result = ex.getResult();
+    public Result exceptionHandler(HttpServletRequest request, HttpServletResponse response, BizException e) throws IOException, ServletException {
+        Result result = e.getResult();
         // resolve i18n
-        String message = MessageUtils.getMessage(result.getMessage(), ex.getParams());
+        String message = MessageUtils.getMessage(result.getMessage(), e.getParams());
         result.setMessage(message);
 
         if (log.isErrorEnabled()) {
-            this.logStackTrace(ex);
+            log.error(e.getMessage(), e);
         }
 
         if (HttpServletRequestUtils.isAjaxRequest(request)) {
@@ -76,75 +74,75 @@ public class ApiExceptionHandler {
 
     /**
      * 文件上传超过最大限制
-     * @param ex
+     * @param e
      * @return
      */
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public Result maxUploadSizeExceededExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) throws IOException, ServletException {
-        return exceptionHandler(request, response, ex, 5001, "文件大小不能超出5M", null);
+    public Result maxUploadSizeExceededExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException, ServletException {
+        return exceptionHandler(request, response, e, 5001, "文件大小不能超出5M", null);
     }
 
 
     /**
      * 未授权异常
-     * @param ex
+     * @param e
      * @return
      */
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public Result accessDeniedHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) throws IOException, ServletException {
-        return exceptionHandler(request, response, ex, ResultCode.ACCESS_FORBIDDEN_ERROR);
+    public Result accessDeniedHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException, ServletException {
+        return exceptionHandler(request, response, e, ResultCode.ACCESS_FORBIDDEN_ERROR);
     }
 
     /**
      * 验证失败
      * @param request
      * @param response
-     * @param ex
+     * @param e
      * @return
      * @throws IOException
      * @throws ServletException
      */
     @ExceptionHandler({BindException.class, ConstraintViolationException.class, IllegalArgumentException.class, MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Result methodArgumentNotValidExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) throws IOException, ServletException {
-        if (ex instanceof ConstraintViolationException) {
-            ConstraintViolationException cve = (ConstraintViolationException) ex;
-            return exceptionHandler(request, response, ex, ResultCode.ARGUMENT_NOT_VALID, formatErrors(cve.getConstraintViolations()));
-        } else if (ex instanceof BindException) {
-            BindException be = (BindException) ex;
-            return exceptionHandler(request, response, ex, ResultCode.ARGUMENT_NOT_VALID, formatErrors(be.getAllErrors()));
-        } else if (ex instanceof IllegalArgumentException) {
-            return exceptionHandler(request, response, ex, ResultCode.ARGUMENT_NOT_VALID, ex.getMessage());
+    public Result methodArgumentNotValidExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException, ServletException {
+        if (e instanceof ConstraintViolationException) {
+            ConstraintViolationException cve = (ConstraintViolationException) e;
+            return exceptionHandler(request, response, e, ResultCode.ARGUMENT_NOT_VALID, formatErrors(cve.getConstraintViolations()));
+        } else if (e instanceof BindException) {
+            BindException be = (BindException) e;
+            return exceptionHandler(request, response, e, ResultCode.ARGUMENT_NOT_VALID, formatErrors(be.getAllErrors()));
+        } else if (e instanceof IllegalArgumentException) {
+            return exceptionHandler(request, response, e, ResultCode.ARGUMENT_NOT_VALID, e.getMessage());
         }
 
-        MethodArgumentNotValidException me = (MethodArgumentNotValidException) ex;
-        return exceptionHandler(request, response, ex, ResultCode.ARGUMENT_NOT_VALID, formatErrors(me.getBindingResult().getAllErrors()));
+        MethodArgumentNotValidException me = (MethodArgumentNotValidException) e;
+        return exceptionHandler(request, response, e, ResultCode.ARGUMENT_NOT_VALID, formatErrors(me.getBindingResult().getAllErrors()));
     }
 
     /**
      * 未知异常
-     * @param ex
+     * @param e
      * @return
      */
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Result elseExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex) throws IOException, ServletException {
-        return exceptionHandler(request, response, ex, ResultCode.ERROR);
+    public Result elseExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException, ServletException {
+        return exceptionHandler(request, response, e, ResultCode.ERROR);
     }
 
-    private Result exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex, ResultCode resultCode) throws ServletException, IOException {
-        return exceptionHandler(request, response, ex, resultCode, null);
+    private Result exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e, ResultCode resultCode) throws ServletException, IOException {
+        return exceptionHandler(request, response, e, resultCode, null);
     }
 
-    private Result exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex, ResultCode resultCode, Object data) throws ServletException, IOException {
-        return exceptionHandler(request, response, ex, resultCode.getCode(), resultCode.getMessage(), data);
+    private Result exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e, ResultCode resultCode, Object data) throws ServletException, IOException {
+        return exceptionHandler(request, response, e, resultCode.getCode(), resultCode.getMessage(), data);
     }
 
-    private Result exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception ex, int code, String message, Object data) throws ServletException, IOException {
+    private Result exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e, int code, String message, Object data) throws ServletException, IOException {
         if (log.isErrorEnabled()) {
-            this.logStackTrace(ex);
+            log.error(e.getMessage(), e);
         }
 
         if (HttpServletRequestUtils.isAjaxRequest(request) || request.getRequestURI().equals("/documents/upload")) {
@@ -154,15 +152,6 @@ public class ApiExceptionHandler {
         request.setAttribute("javax.servlet.error.status_code", code);
         request.getRequestDispatcher("/error").forward(request, response);
         return null;
-    }
-
-    private void logStackTrace(Exception  ex) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ex.printStackTrace(new PrintStream(baos));
-        String exception = baos.toString();
-        if (log.isErrorEnabled()) {
-            log.error(exception);
-        }
     }
 
     private List<Map<String, Object>> formatErrors(List<ObjectError> objectErrors) {
