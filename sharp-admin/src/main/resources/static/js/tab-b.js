449a576589adcebd0336469378b4448c439fe7e6
rick <jkxyx205@163.com>
2025-06-12 15:58:30 +0800

    feat: 代码优化

--- sharp-admin/src/main/resources/static/js/tab-b.js
+++ sharp-admin/src/main/resources/static/js/tab-b.js
@@ -3,8 +3,7 @@ import add from './add.js';
 document.getElementById('btn').addEventListener('click', function() {
     document.getElementById('h1').style.color =
         document.getElementById('h1').style.color === 'blue' ? 'red' : 'blue';
-    debugger
-    console.log('Rick', "...")
+    console.log('use add import from add.js', "...")
     let a = Number.parseInt(document.getElementById('a').value);
     let b = Number.parseInt(document.getElementById('b').value);
     document.getElementById('result').innerText = add(a, b);
