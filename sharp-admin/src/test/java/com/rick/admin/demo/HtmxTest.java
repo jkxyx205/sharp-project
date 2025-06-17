c11f4cb116e1ae0e3650cb584d77f15dc469d1e7
rick <jkxyx205@163.com>
2025-06-18 01:19:52 +0800

    test: adminLTE 后台管理模版

--- sharp-admin/src/test/java/com/rick/admin/demo/HtmxTest.java
+++ sharp-admin/src/test/java/com/rick/admin/demo/HtmxTest.java
@@ -56,4 +56,38 @@ public class HtmxTest {
                 .sord(SordEnum.ASC)
                 .build());
     }
+
+    @Test
+    public void testReportAdminLTE() {
+        reportService.saveOrUpdate(Report.builder()
+                .id(966383991991062528L)
+                .code("sys_dict-adminlte")
+                .tplName("adminlte/list")
+                .name("字典管理-adminlte")
+                .reportAdviceName("dictHtmxReportAdvice")
+                .summaryColumnNames("sort")
+                .additionalInfo(Params.builder(2).pv("formId", "695312747063197696")
+                        // 1. link
+//                        .pv("formAction", "link")
+//                        .pv("formPage", "demos/htmx/form")
+                        // 2. drawer
+                        .pv("formAction", "drawer")
+                        // .pv("formPage", "tpl/form/form-full") // drawer 默认使用 tpl/form/form-full，从 form tplName 继承
+                        .pv("select", true).build())
+                .querySql("select id, type, name, label, sort from sys_dict where type = :type and is_deleted = 0 order by type, sort asc")
+                .queryFieldList(Arrays.asList(
+                        new QueryField("type", "分类", QueryField.Type.SELECT, "sys_dict_type")
+                ))
+                .reportColumnList(Arrays.asList(
+                        new HiddenReportColumn("id"),
+                        new ReportColumn("name", "编码"),
+                        new ReportColumn("label", "显示值"),
+                        new ReportColumn("type", "分类", true),
+                        new ReportColumn("sort", "排序号").setColumnWidth(30)
+                ))
+                .pageable(true)
+                .sidx("id")
+                .sord(SordEnum.ASC)
+                .build());
+    }
 }
