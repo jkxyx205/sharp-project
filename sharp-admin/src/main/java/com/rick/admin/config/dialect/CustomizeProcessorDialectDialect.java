364bf868eaf2a864ff93889a275d1baee9b1a53d
rick <jkxyx205@163.com>
2025-07-29 19:50:02 +0800

    chore: 单图片上传控件

--- sharp-admin/src/main/java/com/rick/admin/config/dialect/CustomizeProcessorDialectDialect.java
+++ sharp-admin/src/main/java/com/rick/admin/config/dialect/CustomizeProcessorDialectDialect.java
@@ -50,6 +50,7 @@ public class CustomizeProcessorDialectDialect extends AbstractProcessorDialect {
         processors.add(new SelectOptionProcessor(DIALECT_PREFIX, dictService, sharpService));
         processors.add(new SharpFormProcessor(DIALECT_PREFIX, formService));
         processors.add(new FileUploadProcessor(DIALECT_PREFIX));
+        processors.add(new ImageUploadProcessor(DIALECT_PREFIX));
         processors.add(new DataAttributeTagProcessor(DIALECT_PREFIX));
         processors.add(new CodeInputProcessor(DIALECT_PREFIX, codeInputService));
         processors.add(new SpanProcessor(DIALECT_PREFIX, dictService));
