cba61e051fa874534a357bf85e348fe1c15ee7aa
rick <jkxyx205@163.com>
2025-08-26 04:30:36 +0800

    chore: 完善 SQLiteDialect

--- sharp-database2/src/main/java/com/rick/db/repository/support/dialect/SQLiteDialect.java
+++ sharp-database2/src/main/java/com/rick/db/repository/support/dialect/SQLiteDialect.java
@@ -2,6 +2,9 @@ package com.rick.db.repository.support.dialect;
 
 import com.rick.db.plugin.page.PageModel;
 import com.rick.db.repository.model.DatabaseType;
+import org.apache.commons.lang3.StringUtils;
+
+import static com.rick.db.repository.support.Constants.GROUP_DUMMY_TABLE_NAME;
 
 /**
  * @author Rick.Xu
@@ -11,17 +14,30 @@ public class SQLiteDialect extends AbstractDialect {
 
     @Override
     public String pageSql(String sql, PageModel model) {
-        return null;
+        StringBuilder sb = new  StringBuilder();
+        sb.append("SELECT * FROM ")
+                .append("(").append(sql).append(") ").append(GROUP_DUMMY_TABLE_NAME);
+
+        if (model != null && (StringUtils.isNotBlank(model.getSidx()) && StringUtils
+                .isNotBlank(model.getSord()))) {
+            sb.append(" ORDER BY ").append(model.getSidx()).append(" ")
+                    .append(model.getSord());
+        }
+
+        if (model != null)
+            sb.append(" LIMIT ").append((model.getPage()-1) *  model.getSize()).append(",").append(model.getSize());
+
+        return sb.toString();
     }
 
     @Override
     public String contactString(String name) {
-        return null;
+        return " CONCAT('%',UPPER(:" + name + "),'%')";
     }
 
     @Override
     public String escapeString() {
-        return null;
+        return "escape '\\'";
     }
 
     @Override
