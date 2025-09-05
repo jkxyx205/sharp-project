a1e3c7f6a04d6d2baf4736d96b4b8b1e0a1650d1
rick <jkxyx205@163.com>
2025-09-06 07:19:25 +0800

    test: test

--- mvn.sh
+++ mvn.sh
@@ -3,17 +3,17 @@
 PROJECTS=(
 sharp-dependencies
 sharp-common
-sharp-database
+#sharp-database
 sharp-database2
-sharp-excel
-sharp-meta
-sharp-report
-sharp-fileupload
-sharp-formflow
-sharp-mail
-sharp-generator
-sharp-sms
-sharp-notification
+#sharp-excel
+#sharp-meta
+#sharp-report
+#sharp-fileupload
+#sharp-formflow
+#sharp-mail
+#sharp-generator
+#sharp-sms
+#sharp-notification
 )
 
 for p in ${PROJECTS[@]}
