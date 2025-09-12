3ac4e6d15159fdfeeff8d47bd41890a4e0be3eb3
rick <jkxyx205@163.com>
2025-09-12 11:12:25 +0800

    refactor: 代码优化

--- mvn.sh
+++ mvn.sh
@@ -5,15 +5,15 @@ sharp-dependencies
 sharp-common
 #sharp-database
 sharp-database2
-#sharp-excel
-#sharp-meta
-#sharp-report
-#sharp-fileupload
-#sharp-formflow
-#sharp-mail
-#sharp-generator
-#sharp-sms
-#sharp-notification
+sharp-excel
+sharp-meta
+sharp-report
+sharp-fileupload
+sharp-formflow
+sharp-mail
+sharp-generator
+sharp-sms
+sharp-notification
 )
 
 for p in ${PROJECTS[@]}
