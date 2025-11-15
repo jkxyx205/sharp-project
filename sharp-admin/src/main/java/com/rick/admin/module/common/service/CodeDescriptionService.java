55ca01382039a2d4c90ac7d56d1c1c6021c51785
rick <jkxyx205@163.com>
2025-11-16 01:15:04 +0800

    refactor: 优化

--- sharp-admin/src/main/java/com/rick/admin/module/common/service/CodeDescriptionService.java
+++ sharp-admin/src/main/java/com/rick/admin/module/common/service/CodeDescriptionService.java
@@ -3,14 +3,18 @@ package com.rick.admin.module.common.service;
 import com.rick.admin.module.common.entity.CodeDescription;
 import com.rick.common.util.Maps;
 import com.rick.db.repository.EntityCodeDAO;
+import com.rick.db.util.OperatorUtils;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Rick.Xu
@@ -24,9 +28,18 @@ public class CodeDescriptionService {
 
     EntityCodeDAO<CodeDescription, Long> codeDescriptionDAO;
 
-    public void saveAll(CodeDescription.CategoryEnum category, Collection<CodeDescription> list) {
+    public void saveAll(@NotNull CodeDescription.CategoryEnum category, Collection<CodeDescription> list) {
+        if (CollectionUtils.isNotEmpty(list)) {
+            for (CodeDescription codeDescription : list) {
+                codeDescription.setCategory(category);
+            }
+        }
         // TODO
-//        codeDescriptionDAO.insertOrUpdate(list, "category", category.getCode());
+        codeDescriptionDAO.insertOrUpdate(list, "category", category.getCode());
+    }
+
+    public Optional<CodeDescription> findOne(CodeDescription.CategoryEnum category, String code) {
+        return OperatorUtils.expectedAsOptional(codeDescriptionDAO.select("category = :category AND code = :code", Maps.of("category", category.getCode(), "code", code)));
     }
 
     public List<CodeDescription> findAll(CodeDescription.CategoryEnum category) {
