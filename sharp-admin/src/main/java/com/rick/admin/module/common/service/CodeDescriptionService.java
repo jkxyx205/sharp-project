8acd52c3f85e45a9dd95e4a994dc66b1f65474b9
rick <jkxyx205@163.com>
2025-11-16 01:17:53 +0800

    refactor: 优化

--- sharp-admin/src/main/java/com/rick/admin/module/common/service/CodeDescriptionService.java
+++ sharp-admin/src/main/java/com/rick/admin/module/common/service/CodeDescriptionService.java
@@ -3,6 +3,7 @@ package com.rick.admin.module.common.service;
 import com.rick.admin.module.common.entity.CodeDescription;
 import com.rick.db.plugin.dao.core.EntityCodeDAO;
 import com.rick.db.service.support.Params;
+import com.rick.db.util.OptionalUtils;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -11,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Rick.Xu
@@ -26,6 +28,10 @@ public class CodeDescriptionService {
 
     public void saveAll(CodeDescription.CategoryEnum category, Collection<CodeDescription> list) {
         codeDescriptionDAO.insertOrUpdate(list, "category", category.getCode());
+    }
+
+    public Optional<CodeDescription> findOne(CodeDescription.CategoryEnum category, String code) {
+        return OptionalUtils.expectedAsOptional(codeDescriptionDAO.selectByParams(Params.builder(1).pv("category", category.getCode()).pv("code", code).build()));
     }
 
     public List<CodeDescription> findAll(CodeDescription.CategoryEnum category) {
