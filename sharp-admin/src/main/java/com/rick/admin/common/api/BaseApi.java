36f16f905def637e198e01ede8178db9d4ce8625
rick <jkxyx205@163.com>
2025-11-16 01:06:46 +0800

    refactor: 优化

--- sharp-admin/src/main/java/com/rick/admin/common/api/BaseApi.java
+++ sharp-admin/src/main/java/com/rick/admin/common/api/BaseApi.java
@@ -11,13 +11,14 @@ import com.rick.db.plugin.dao.core.EntityDAO;
 import com.rick.db.plugin.dao.core.EntityDAOManager;
 import com.rick.db.service.BaseServiceImpl;
 import com.rick.db.service.SharpService;
+import com.rick.db.util.OptionalUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Rick.Xu
@@ -43,14 +44,26 @@ public class BaseApi<S extends BaseServiceImpl<? extends EntityDAO<T, ID>, T, ID
     @GetMapping
     public Grid<Map<String, Object>> list(HttpServletRequest request) {
         Map<String, Object> params = HttpServletRequestUtils.getParameterMap(request);
-        return GridUtils.list(entityDAO.getSelectConditionSQL(params), params);
+        Grid<Map<String, Object>> grid = GridUtils.list(entityDAO.getSelectConditionSQL(params), params);
+//        return GridUtils.list(SQLParamCleaner.formatSql(entityDAO.getTableMeta().getSelectConditionSQL(), params, new HashMap<>()), params);
+
+        List<Map<String, Object>> formatList = grid.getRows().stream().map(row -> flattenKeys(row)).collect(Collectors.toList());
+        grid.getRows().clear();
+        grid.getRows().addAll(formatList);
+        return grid;
     }
 
     @GetMapping("one")
-    public Map<String, Object> one(HttpServletRequest request) {
+    public T one(HttpServletRequest request) {
         Map<String, Object> params = HttpServletRequestUtils.getParameterMap(request);
-        return sharpService.queryForObject(entityDAO.getSelectConditionSQL(params), params).orElseThrow(() -> getResourceNotFoundException(null));
+        return getEntityFromOptional(OptionalUtils.expectedAsOptional(entityDAO.selectByParams(params)), params);
     }
+
+//    @GetMapping("one")
+//    public Map<String, Object> one(HttpServletRequest request) {
+//        Map<String, Object> params = HttpServletRequestUtils.getParameterMap(request);
+//        return sharpService.queryForObject(entityDAO.getSelectConditionSQL(params), params).orElseThrow(() -> getResourceNotFoundException(null));
+//    }
 
 //    @PostMapping
 //    public SimpleEntity save(@RequestBody T t) {
@@ -108,5 +121,37 @@ public class BaseApi<S extends BaseServiceImpl<? extends EntityDAO<T, ID>, T, ID
 
     protected String comment() {
         return EntityDAOManager.getTableMeta(entityDAO.getEntityClass()).getTable().comment();
+    }
+
+    public static Map<String, Object> flattenKeys(Map<String, Object> source) {
+        Map<String, Object> result = new HashMap<>();
+
+        for (Map.Entry<String, Object> entry : source.entrySet()) {
+            String key = entry.getKey();
+
+            if (key.contains(".")) {
+                String[] parts = key.split("\\.");
+                if (parts.length == 2) {
+                    String obj = parts[0];
+                    String field = parts[1];
+                    String fieldCamel = Character.toUpperCase(field.charAt(0)) + field.substring(1);
+
+                    key = obj + fieldCamel; // 替换 key
+                }
+            }
+
+            Object value = entry.getValue();
+            if (Objects.nonNull(value)) {
+//                if (value instanceof PGobject pgObject) {
+//                    String value1 = pgObject.getValue();
+//                    value = JsonUtils.toJsonNode(value1);
+//                }
+
+                result.put(key, value);
+            }
+
+        }
+
+        return result;
     }
 }
